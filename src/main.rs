fe4e5895635a80384fb494fc25f309e8fdc1eedd
Artur Socha <asocha@redhat.com>
2020-08-11 20:15:56 +0200

    First example works, can connect to irc server and receive messages

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,24 @@
-use futures::prelude::*;
 use irc::client::prelude::*;
+use futures::prelude::*;
 
 #[tokio::main]
-async fn main() -> irc::error::Result<()> {
+async fn main() -> Result<(), failure::Error> {
+    // We can also load the Config at runtime via Config::load("path/to/config.toml")
     let config = Config {
         nickname: Some("asochatst".to_owned()),
         server: Some("chat.freenode.net".to_owned()),
         channels: vec!["#asochatst".to_owned()],
-        use_tls: Some(false),
-        ..Default::default()
+        ..Config::default()
     };
-    
+
     let mut client = Client::from_config(config).await?;
     client.identify()?;
 
     let mut stream = client.stream()?;
-    let sender = client.sender();
 
     while let Some(message) = stream.next().await.transpose()? {
         print!("{}", message);
-
-        match message.command {
-            Command::PRIVMSG(ref target, ref msg) => {
-                sender.send_privmsg(target, "hello")?;
-            }
-            _ => (),
-        }
     }
 
-    Ok(());
+    Ok(())
 }
