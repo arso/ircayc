f95c67d530e4a8393291e51beacc6ddeb394d75f
Artur Socha <asocha@redhat.com>
2020-08-31 21:49:50 +0200

    Refactoring to extract command specific actions

---
+++ src/actions.rs
@@ -0,0 +1,28 @@
+use std::process::Command;
+
+use irc::client::prelude::*;
+
+pub trait Executable {
+    fn execute(&self) -> String;
+}
+
+pub struct ListDir { pub arguments: String, pub msg: Message }
+
+pub struct OpenSSHTunnel {
+    pub arguments: String
+}
+
+pub struct ConnectVPN {
+    pub arguments: String
+}
+
+impl Executable for ListDir {
+    fn execute(&self) -> String {
+        let dirs = Command::new("ls")
+            .args(&["l","a",&self.arguments])
+            .output()
+            .expect("failed to execute process");
+        println!("status: {}", dirs.status);
+        String::from_utf8(dirs.stdout).unwrap()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-use irc::client::prelude::*;
+extern crate irc;
+
+use std::borrow::Borrow;
+
 use futures::prelude::*;
-use std::process::Command;
-use std::io::{self, Write};
+use irc::client::prelude::*;
+
+use crate::actions::{Executable, ListDir};
+
+mod actions;
 
 #[tokio::main]
 async fn main() -> Result<(), failure::Error> {
+
     // We can also load the Config at runtime via Config::load("path/to/config.toml")
     let config = Config {
         nickname: Some("asochatst".to_owned()),
@@ -12,33 +19,23 @@ async fn main() -> Result<(), failure::Error> {
         channels: vec!["#asochatst".to_owned()],
         ..Config::default()
     };
-
     let mut client = Client::from_config(config).await?;
     client.identify()?;
 
+
     let mut stream = client.stream()?;
-
     while let Some(message) = stream.next().await.transpose()? {
-        // TODO
-        // - react on particular command messages on irc 
-        // - decrypt message content (alg tbd)
-        // - check if vpn connection is open and if not 
-        //    use decrypted token to initiate vpn connection (all via shell Command::new())
-        //    ircayc must not use 'rootish' permissions
-        // - open ssh tunnel to preconfigured host(.ssh/config + public key auth)
-        // 
-        // other things: 
-        // - systemd service
-        // - all configuration at $HOME/.ircyayc/config  with proper rights (consider encryption)
-
-
-        // this is just a PoC
-        let dirs = Command::new("ls").output().expect("failed to execute process");
-       
-        println!("status: {}", dirs.status);
-        io::stdout().write_all(&dirs.stdout).unwrap();
-        io::stderr().write_all(&dirs.stderr).unwrap();
-        print!("{}", message);
+        match message.command {
+            Command::PRIVMSG(ref channel, ref message_txt) => {
+                if message_txt.contains("LSDIRS") {
+                    let list_dir = ListDir { arguments: "/home/asocha".to_string(), msg: message.to_owned() };
+                    let result = list_dir.execute();
+                    print!("{}", &*result);
+                    client.send_privmsg(&channel, "ACK: ".to_owned() + result.as_ref()).unwrap();
+                }
+            }
+            _ => (),
+        }
     }
 
     Ok(())
